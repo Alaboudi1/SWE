d18322f8a77b2b62c38f2eab96d80224f6c4a7f5
abdulaziz alaboudi <abdulaziz alaboudi>
2016-12-13 20:42:29 +0300

    .

---
+++ PHP/lab 10/operations.php
@@ -0,0 +1,43 @@
+<?php
+
+
+ 
+class operations{
+  function __construct($db,$user){
+    $this->db=$db;
+    $this->user=$user;
+  }
+  
+  function auth(){
+    try{
+     $connection = $this->db->connection();
+     $statment=$connection->prepare("SELECT id FROM Login WHERE UserName = ? AND Password = ?");
+     $user= $this->user->getUsername();
+     $password = $this->user->getPassword();
+     $statment->bindParam(1,$user);
+     $statment->bindParam(2, $password);     
+     $statment->execute();
+     $this->db->close();
+    }
+    catch(PODException $e){
+      echo $e->getMessage();
+      $db->close();
+      return;
+    }
+     return $statment->fetch(PDO::FETCH_NUM) != null;
+
+  }
+  
+  function createUser(){
+    //TODO
+  }
+  function removeUser(){
+    //TODO
+  }
+  function updateUser($newUsername,$newPassword){
+    //TODOO
+  }
+}
+
+
+?>
